b2b8a298f511b8420a906acac9e751d7128b2a1d
jenniferhellar <jenniferhellar@gmail.com>
2022-03-22 14:39:57 -0500

    Combining const and extract_seg for both datasets
    Converted const.py to function-based with dataset-specific returns.  Replaced tb_extract_seg.py with extract_seg.py, works for both chb-mit and kaggle-ieeg datasets

--- const.py
+++ const.py
@@ -2,70 +2,133 @@ import os
 import platform
 
 
-mySys = platform.system()
+DATASETS = ['chb-mit', 'kaggle-ieeg']
 
-if mySys == 'Linux':
-	usr = os.environ.get('USER')
-	CODEDIR = '/home/' + usr + '/emdmd'
-	if usr == 'jlh24':
-		WORKDIR = '/media/large_disk/jlh24/emdmd'
+
+def get_dirs(dataset):
+	mySys = platform.system()
+
+	if mySys == 'Linux':
+		usr = os.environ.get('USER')
+		CODEDIR = '/home/' + usr + '/emdmd'
+		if usr == 'jlh24':
+			WORKDIR = '/media/large_disk/jlh24/emdmd'
+		else:
+			WORKDIR = CODEDIR
+	elif mySys == 'Windows':
+		CODEDIR =  'D:\\EmDMD\\emdmd'
+		WORKDIR = 'D:\\EmDMD\\emdmd'	
 	else:
-		WORKDIR = CODEDIR
-	DATADIR = os.path.join(WORKDIR, 'data', 'physionet.org/files/chbmit/1.0.0')
-elif mySys == 'Windows':
-	CODEDIR =  'D:\\EmDMD\\emdmd'
-	WORKDIR = 'D:\\EmDMD\\emdmd'
-	DATADIR = os.path.join(WORKDIR, 'data', 'chb-mit')
-else:
-	print('\nERROR: unknown operating system. Must specify workDir and dataDir in const.py\n')
-	exit(1)
+		print('\nERROR: unknown operating system. Must specify CODEDIR and WORKDIR in const.py.\n')
+		exit(1)
 
-RESDIR = os.path.join(CODEDIR, 'tempdata', 'chb-mit')
-LOGDIR = os.path.join(CODEDIR, 'logs')
+	DATADIR = os.path.join(WORKDIR, 'data', dataset)
+	if not os.path.isdir(DATADIR):
+		print('\n\nERROR: unable to locate data. Expected to find the dataset at ', DATADIR)
+		exit(1)
 
-FS = 256
+	RESDIR = os.path.join(CODEDIR, 'tempdata', dataset)
+	if not os.path.isdir(RESDIR):
+		os.makedirs(RESDIR)
 
-PRE_T = 60*60*FS		# 60 min * 60 sec/min * fs samples/sec prior to seizure
-POST_T = 60*60*FS		# 1 hour after seizure end
-INTER_T = 4*60*60*FS	# 4 hours from any seizure
-W = 30*FS 				# window = 30 sec * fs samples/sec
+	LOGDIR = os.path.join(CODEDIR, 'logs')
+	if not os.path.isdir(LOGDIR):
+		os.makedirs(LOGDIR)
 
-R = 40					# SVD truncation parameter, see tb_estimate_r.py
+	return DATADIR, RESDIR, LOGDIR
 
-SEED = 42
 
-normal_ch = [i for i in range(18)]
-alt_ch = [0,1,2,3,5,6,7,8,13,14,15,16,18,19,20,21,10,11]
-CHANNELS = {
-	'chb01': {'general':normal_ch},
-	'chb02': {'general':normal_ch},
-	'chb03': {'general':normal_ch},
-	'chb04': {'general':normal_ch},
-	'chb05': {'general':normal_ch},
-	'chb06': {'general':normal_ch},
-	'chb07': {'general':normal_ch},
-	'chb08': {'general':normal_ch},
-	'chb09': {'general':normal_ch},
-	'chb10': {'general':normal_ch},
-	'chb11': {'chb11_01.edf':normal_ch,
-			'general':alt_ch},
-	'chb13': {'general':alt_ch},
-	'chb14': {'general':alt_ch},
-	'chb15': {'chb15_01.edf':alt_ch,
-			'general':[0,1,2,3,5,6,7,8,14,15,16,17,19,20,21,22,10,11]},
-	'chb16': {'general':alt_ch},
-	'chb17': {'general':alt_ch},
-	'chb18': {'general':alt_ch},
-	'chb19': {'general':alt_ch},
-	'chb20': {'general':alt_ch},
-	'chb21': {'general':alt_ch},
-	'chb22': {'general':alt_ch},
-	'chb23': {'general':normal_ch},
-	'chb24': {'general':normal_ch}
-}
+def get_fs(dataset):
+	if dataset == 'chb-mit':
+		return 256
+	elif dataset == 'kaggle-ieeg':
+		return 5000
 
-PATIENTS = ['chb01', 'chb02', 'chb04', 'chb05', 'chb06', 'chb07', 'chb09',
-			'chb10', 'chb13', 'chb14', 'chb16', 'chb17', 'chb18', 'chb20',
-			'chb22', 'chb23']
 
-# PATIENTS = ['chb01']
+def get_patients(dataset):
+	if dataset == 'chb-mit':
+		PATIENTS = ['chb01', 'chb02', 'chb04', 'chb05', 'chb06', 'chb07', 'chb09',
+				'chb10', 'chb13', 'chb14', 'chb16', 'chb17', 'chb18', 'chb20',
+				'chb22', 'chb23']
+	elif dataset == 'kaggle-ieeg':
+		PATIENTS = ['Patient_1', 'Patient_2']
+
+	return PATIENTS
+
+
+def get_channels(dataset):
+	if dataset == 'chb-mit':
+		normal_ch = [i for i in range(18)]
+		alt_ch = [0,1,2,3,5,6,7,8,13,14,15,16,18,19,20,21,10,11]
+		CHANNELS = {
+			'chb01': {'general':normal_ch},
+			'chb02': {'general':normal_ch},
+			'chb03': {'general':normal_ch},
+			'chb04': {'general':normal_ch},
+			'chb05': {'general':normal_ch},
+			'chb06': {'general':normal_ch},
+			'chb07': {'general':normal_ch},
+			'chb08': {'general':normal_ch},
+			'chb09': {'general':normal_ch},
+			'chb10': {'general':normal_ch},
+			'chb11': {'chb11_01.edf':normal_ch,
+					'general':alt_ch},
+			'chb13': {'general':alt_ch},
+			'chb14': {'general':alt_ch},
+			'chb15': {'chb15_01.edf':alt_ch,
+					'general':[0,1,2,3,5,6,7,8,14,15,16,17,19,20,21,22,10,11]},
+			'chb16': {'general':alt_ch},
+			'chb17': {'general':alt_ch},
+			'chb18': {'general':alt_ch},
+			'chb19': {'general':alt_ch},
+			'chb20': {'general':alt_ch},
+			'chb21': {'general':alt_ch},
+			'chb22': {'general':alt_ch},
+			'chb23': {'general':normal_ch},
+			'chb24': {'general':normal_ch}
+		}
+	elif dataset == 'kaggle-ieeg':
+		dog_ch = [i for i in range(16)]
+		CHANNELS = {
+			'Patient_1': {'general':[i for i in range(15)]},
+			'Patient_2': {'general':[i for i in range(24)]},
+			'Dog_1': {'general':dog_ch},
+			'Dog_2': {'general':dog_ch},
+			'Dog_3': {'general':dog_ch},
+			'Dog_4': {'general':dog_ch},
+			'Dog_5': {'general':dog_ch}
+		}
+
+	return CHANNELS
+
+
+def get_details(dataset):
+
+	FS = get_fs(dataset)
+
+	PATIENTS = get_patients(dataset)
+
+	CHANNELS = get_channels(dataset)
+
+	return FS, PATIENTS, CHANNELS
+
+
+def get_emdmd_params(FS):
+	W = 30*FS 				# window = 30 sec * fs samples/sec
+
+	R = 40					# SVD truncation parameter, see tb_estimate_r.py
+
+	SEED = 42
+
+	return W, R, SEED
+
+
+def get_label_rules(FS):
+	# 60 min * 60 sec/min * fs samples/sec prior to seizure
+	preictal_t = 60*60*FS
+	# 1 hour after seizure end
+	postictal_t = 60*60*FS
+	# 4 hours from any seizure
+	interictal_t = 4*60*60*FS
+
+	return preictal_t, postictal_t, interictal_t
---
+++ extract_seg.py
@@ -0,0 +1,450 @@
+import os
+import argparse
+import pickle
+import numpy as np
+import math
+
+import util
+import const
+
+
+parser = argparse.ArgumentParser(
+    description='Processes a single patient to extract preictal and interictal segments.')
+
+parser.add_argument('--dataset', required=True,
+					help='Dataset to process.')
+
+parser.add_argument('--patient', required=True,
+                    help='Patient to process.')
+
+args = parser.parse_args()
+dataset = vars(args)['dataset']
+patient = vars(args)['patient']
+
+if dataset not in const.DATASETS:
+	print('\n\nERROR: unsupported dataset argument. Allowed options: \n', const.DATASETS)
+	exit(1)
+
+FS, PATIENTS, CHANNELS = const.get_details(dataset)
+if patient not in PATIENTS:
+	print('\n\nERROR: unsupported patient argument. Select from allowed options: {} or add new options in const.get_patients()\n'.format(PATIENTS))
+	exit(1)
+
+DATADIR, RESDIR, LOGDIR = const.get_dirs(dataset)
+
+W, _, _ = const.get_emdmd_params(FS)
+PRE_T, POST_T, INTER_T = const.get_label_rules(FS)
+
+if dataset == 'chb-mit':
+	fpath = os.path.join(DATADIR, patient)
+elif dataset == 'kaggle-ieeg':
+	fpath = os.path.join(DATADIR, patient, patient)
+
+outDir = os.path.join(RESDIR, patient)
+if not os.path.isdir(outDir):
+	os.makedirs(outDir)
+
+
+print('\n\nProcessing {} record {} ...'.format(dataset, patient))
+files = os.listdir(fpath)
+
+if dataset == 'chb-mit':
+	ftype = '.edf'
+elif dataset == 'kaggle-ieeg':
+	ftype = '.mat'
+	FILES_PER_HOUR	=	6
+
+datafiles = [i for i in files if i[-4:] == ftype]
+datafiles.sort()
+
+
+# extra processing needed to identify preictal/interictal segments
+if dataset == 'chb-mit':
+	infofile = os.path.join(outDir, '{}_info.pkl'.format(patient))
+	if os.path.exists(infofile):
+		print('\n{} found. Loading record info ...'.format(infofile))
+		with open(infofile, 'rb') as f:
+			info = pickle.load(f)
+		total_len = info[0]
+		total_seizures = info[1]
+		num_seizures = info[2]
+		file_starts = info[3]
+		preictal_seg = info[4]
+		preictal_seg_byfiles = info[5]
+		interictal_seg = info[6]
+		interictal_seg_byfiles = info[7]
+	else:
+		print('\n{} not found. Computing record info ...'.format(infofile))
+		print('\nReading ' + os.path.join(fpath, patient+'-summary.txt') + ' ...')
+		summarypath = os.path.join(fpath, patient+'-summary.txt')
+		infodict = util.read_summary(summarypath)
+
+		file_starts = []
+		seizure_starts = []
+		seizure_stops = []
+
+		baseidx = 0
+
+		first = True
+
+		for f in datafiles:
+			print('\tReading ' + os.path.join(fpath, f) + ' ...')
+			x_all = util.read_edf(os.path.join(fpath, f))
+			if f in infodict:
+				seizures = infodict[f]['s']
+			else:
+				seizures = 0
+			s = 0
+			while s < seizures:
+				seizure_starts.append(infodict[f][s][0] + baseidx)
+				seizure_stops.append(infodict[f][s][1] + baseidx)
+				s += 1
+			file_starts.append(baseidx)
+			baseidx += x_all.shape[1]
+
+		total_len = baseidx
+		print('\n\n')
+		print('Extracting basic file and seizure information ' + ' ...')
+		print('total_len = ' + str(total_len))
+		print('\n')
+		print('file_starts = ', file_starts)
+		print('\n')
+		print('seizure_starts = ', seizure_starts)
+		print('seizure_stops = ', seizure_stops)
+		total_seizures = len(seizure_starts)
+
+		""" Combine any seizures that occur <2 hour apart """
+		print('\n\n')
+		print('Combining seizures <2 hours apart (measured from end of 1st seizure to start of 2nd)' + ' ...')
+		seizurestart = seizure_starts
+		seizurestop = seizure_stops
+
+		i = 0
+		most_recent_seizure = 0
+		while i < len(seizurestart) - 1:
+			curr_seizure = seizurestart[i]
+			if most_recent_seizure == 0:
+				most_recent_seizure = seizurestop[i]
+			
+			next_seizure = seizurestart[i + 1]
+			if (next_seizure - most_recent_seizure) < 2*60*60*256:	# 2 hours
+				most_recent_seizure = seizurestop[i + 1]
+				seizurestart.pop(i+1)
+				seizurestop.pop(i)
+			else:
+				most_recent_seizure = 0
+				i += 1
+
+		print('seizurestart = ', seizurestart)
+		print('seizurestop = ', seizurestop)
+		num_seizures = len(seizurestart)
+		print('num_seizures = ', num_seizures)	# number of seizures for this patient
+
+		print('\n\n')
+		print('Computing preictal, postictal, and interictal transitions ' + ' ...')
+		preictalstart = [max(0, i - PRE_T) for i in seizurestart]
+		postictalend = [i + POST_T for i in seizurestop]
+
+		interstart = [0] + [i + INTER_T for i in seizurestop]
+		interend = [max(0, i - INTER_T) for i in seizurestart] + [total_len]
+
+		print('preictalstart = ', preictalstart)
+		print('postictalend = ', postictalend)
+		print('interstart = ', interstart)
+		print('interend = ', interend)
+
+		"""
+		preictal segments 
+		"""
+		print('\n\n')
+		print('Computing preictal segments ' + ' ...')
+
+		# by global sample indices
+		preictal_seg = [(preictalstart[i], seizurestart[i]) for i in range(len(seizurestart))]
+		print('preictal_seg = ', preictal_seg)
+
+		# by file breaks
+		preictal_seg_byfiles = []
+
+		for i in range(len(preictal_seg)):
+			(start, stop) = preictal_seg[i]
+			for f in range(len(file_starts)):
+				fidx = file_starts[f]
+				if start < fidx:
+					if stop < fidx:
+						preictal_seg_byfiles.append((start, stop))
+					else:
+						preictal_seg_byfiles.append((start, fidx))
+						preictal_seg_byfiles.append((fidx, stop))
+					break
+		print('preictal_seg_byfiles = ', preictal_seg_byfiles)
+
+		"""
+		interictal segments 
+		"""
+		print('\n\n')
+		print('Computing interictal segments ' + ' ...')
+
+		# by global sample indices
+		interictal_seg = [(interstart[i], interend[i]) for i in range(len(interstart)) if interend[i] > interstart[i]]
+		print('interictal_seg = ', interictal_seg)
+
+		# by file breaks
+		interictal_seg_byfiles = []
+
+		for i in range(len(interictal_seg)):
+			(start, stop) = interictal_seg[i]
+
+			if start < file_starts[-1]:
+				startfileidx = next(x for x, val in enumerate(file_starts)
+			                              if val > start)
+				startfileidx -= 1
+			else:
+				startfileidx = len(file_starts) - 1
+
+			stopfileidx = [i for i in range(len(file_starts)) if file_starts[i] > stop]
+			if stopfileidx != []:
+				stopfileidx = stopfileidx[0] - 1
+			else:
+				stopfileidx = len(file_starts)-1
+
+			if stopfileidx > startfileidx:	# across multiple files
+				interictal_seg_byfiles.append((start, file_starts[startfileidx+1]))
+				for j in range(startfileidx+1, stopfileidx):
+					interictal_seg_byfiles.append((file_starts[j], file_starts[j+1]))
+				interictal_seg_byfiles.append((file_starts[stopfileidx], stop))
+			else:
+				interictal_seg_byfiles.append((start, stop))
+
+		print('interictal_seg_byfiles = ', interictal_seg_byfiles)
+
+		print('\n\nSaving all record info to ', infofile)
+		with open(infofile, 'wb') as f:
+			info = [total_len, total_seizures, num_seizures, file_starts,
+					preictal_seg, preictal_seg_byfiles,
+					interictal_seg, interictal_seg_byfiles]
+			pickle.dump(info, f)
+
+
+""" Extract preictal segments and save to file """
+print('\n')
+print('Extracting preictal segments ' + ' ...')
+
+if dataset == 'chb-mit':
+	first = True
+	segidx = 0
+	curr_seg = preictal_seg[segidx]
+	preictal_time = 0
+
+	for (i, j) in preictal_seg_byfiles:
+		if i < file_starts[-1]:
+			startfileidx = next(x for x, val in enumerate(file_starts)
+		                              if val > i)
+			startfileidx -= 1
+		else:
+			startfileidx = len(file_starts) - 1
+		
+		filebasesamp = file_starts[startfileidx]
+		filei = i - filebasesamp	# segment start index within file
+		filej = j - filebasesamp	# segment stop index within file
+
+		f = datafiles[startfileidx]
+		print('\tReading ' + os.path.join(fpath, f) + ' ...')
+		x_all = util.read_edf(os.path.join(fpath, f))
+		if f in CHANNELS[patient].keys():
+			ch = CHANNELS[patient][f]
+		else:
+			ch = CHANNELS[patient]['general']
+		if first:
+			preictal = x_all[ch, filei:filej]	# first 18 channels
+			first = False 
+		else:
+			# concatenate horizontally
+			preictal = np.hstack((preictal, x_all[ch, filei:filej]))
+
+		curr_seg = preictal_seg[segidx]
+		if j == curr_seg[1]:
+			print('\tFiltering ' + ' ...\n')
+			preictal = util.butter_bandpass_filter(preictal, fs=FS)
+			# truncate to a round number of minutes
+			cut = math.floor(preictal.shape[1]/(FS*60))*(FS*60)
+			preictal = preictal[:, -cut:]
+			# save to file
+			output = open(os.path.join(outDir, patient + '_preictal{}.pkl'.format(segidx)), 'wb')
+			pickle.dump(preictal, output)
+			output.close()
+
+			print('\tFinished segment {} (#{} of {})'.format(curr_seg, segidx+1, len(preictal_seg)))
+			print('\t\tsegment shape ', np.shape(preictal), '\n')
+			preictal_time += np.shape(preictal)[1]/FS	# in seconds
+			first = True
+			segidx += 1
+
+elif dataset == 'kaggle-ieeg':
+	preictalfiles = [i for i in datafiles if i.find('preictal') != -1]
+	num_seizures = len(preictalfiles) / FILES_PER_HOUR
+	n_segs = math.ceil(len(preictalfiles)/FILES_PER_HOUR)
+
+	cnt = 0
+	segidx = 0
+	preictal_time = 0
+	for f in preictalfiles:
+		cnt += 1
+
+		print('\tReading ' + os.path.join(fpath, f) + ' ...')
+		x_all, t_sec, fs, channels, s_idx = util.read_mat(os.path.join(fpath, f))
+
+		if f in CHANNELS[patient].keys():
+			ch = CHANNELS[patient][f]
+		else:
+			ch = CHANNELS[patient]['general']
+
+		if cnt == 1:
+			preictal = x_all[ch, :]
+		else:
+			preictal = np.hstack((preictal, x_all[ch, :]))
+
+		if (cnt == FILES_PER_HOUR) or (f == preictalfiles[-1]):
+			print('\tFiltering ...\n')
+			preictal = util.butter_bandpass_filter(preictal, fs=FS)
+			print('\tSaving preictal segment {} of {} ...\n'.format(segidx + 1, n_segs))
+			output = open(os.path.join(outDir, patient + '_preictal{}.pkl'.format(segidx)), 'wb')
+			pickle.dump(preictal, output)
+			output.close()
+			preictal_time += np.shape(preictal)[1]/FS	# in seconds
+			segidx += 1
+
+			cnt = 0
+
+print('done.')
+
+
+""" Extract interictal segments and save to file """
+print('\n')
+print('Extracting interictal segments ...')
+
+if dataset == 'chb-mit':
+	if len(interictal_seg) > 0:
+		first = True
+		segidx = 0
+		fidx = 0
+		curr_seg = interictal_seg[segidx]
+		interictal_time = 0
+
+		# limit files to 1 hour long
+		max_len = 1*60*60*FS
+
+		for (i, j) in interictal_seg_byfiles:
+			if i < file_starts[-1]:
+				startfileidx = next(x for x, val in enumerate(file_starts)
+			                              if val > i)
+				startfileidx -= 1
+			else:
+				startfileidx = len(file_starts) - 1
+
+			filebasesamp = file_starts[startfileidx]
+			filei = i - filebasesamp
+			filej = j - filebasesamp
+
+			f = datafiles[startfileidx]
+			print('\tReading ' + os.path.join(fpath, f) + ' ...')
+			x_all = util.read_edf(os.path.join(fpath, f))
+			if f in CHANNELS[patient].keys():
+				ch = CHANNELS[patient][f]
+			else:
+				ch = CHANNELS[patient]['general']
+			if first:
+				interictal = x_all[ch, filei:filej]
+				first = False
+			else:
+				tmp_interictal = np.hstack((interictal, x_all[ch, filei:filej]))
+				curr_len = tmp_interictal.shape[1]
+				while curr_len > max_len:
+					interictal = tmp_interictal[:, :max_len]
+					print('\tFiltering ' + ' ...\n')
+					interictal = util.butter_bandpass_filter(interictal, fs=FS)
+					# save to file
+					output = open(os.path.join(outDir, patient + '_interictal{}.pickle'.format(fidx)), 'wb')
+					pickle.dump(interictal, output)
+					output.close()
+
+					interictal_time += interictal.shape[1]/FS	# in seconds
+					fidx += 1
+
+					print('\tFinished an hour of segment {} (#{} of {})'.format(curr_seg, segidx+1, len(interictal_seg)))
+					print('\t\tsegment shape ', np.shape(interictal), '\n')
+
+					tmp_interictal = tmp_interictal[:, max_len:]
+					curr_len = tmp_interictal.shape[1]
+				interictal = tmp_interictal
+
+			# check if the end of current interictal segment is reached
+			curr_seg = interictal_seg[segidx]
+			if j == curr_seg[1]:
+				print('\tFiltering ' + ' ...\n')
+				interictal = util.butter_bandpass_filter(interictal, fs=FS)
+				# truncate to a round number of minutes
+				cut = math.floor(interictal.shape[1]/(FS*60))*(FS*60)
+				interictal = interictal[:, -cut:]
+				# save to file
+				output = open(os.path.join(outDir, patient + '_interictal{}.pickle'.format(fidx)), 'wb')
+				pickle.dump(interictal, output)
+				output.close()
+
+				interictal_time += interictal.shape[1]/FS	# in seconds
+				fidx += 1
+
+				print('\tFinished segment {} (#{} of {})'.format(curr_seg, segidx+1, len(interictal_seg)))
+				print('\t\tsegment shape ', np.shape(interictal), '\n')
+
+				segidx += 1
+				first = True
+
+	else:
+		print('\n\n')
+		print('No interictal segments to extract ' + ' ...')
+		interictal_time = 0
+
+elif dataset == 'kaggle-ieeg':
+	interictalfiles = [i for i in datafiles if i.find('interictal') != -1]
+	n_segs = math.ceil(len(interictalfiles)/FILES_PER_HOUR)
+
+	cnt = 0
+	segidx = 0
+	interictal_time = 0
+	for f in interictalfiles:
+		cnt += 1
+
+		print('\t\tReading ' + os.path.join(fpath, f) + ' ...')
+		x_all, t_sec, fs, channels, s_idx = util.read_mat(os.path.join(fpath, f))
+		if f in CHANNELS[patient].keys():
+			ch = CHANNELS[patient][f]
+		else:
+			ch = CHANNELS[patient]['general']
+
+		if cnt == 1:
+			interictal = x_all[ch, :]
+		else:
+			interictal = np.hstack((interictal, x_all[ch, :]))
+
+		if (cnt == FILES_PER_HOUR) or (f == interictalfiles[-1]):
+			print('\tFiltering ...\n')
+			interictal = util.butter_bandpass_filter(interictal, fs=FS)
+			print('\tSaving interictal segment {} of {} ...\n'.format(segidx + 1, n_segs))
+			output = open(os.path.join(outDir, patient + '_interictal{}.pickle'.format(segidx)), 'wb')
+			pickle.dump(interictal, output)
+			output.close()
+			interictal_time += np.shape(interictal)[1]/FS	# in seconds
+			segidx += 1
+
+			cnt = 0
+
+print('done.')
+
+
+print('\n\nOutput files can be found in {}\n'.format(outDir))
+if dataset == 'chb-mit':
+	print('total_seizures = ', total_seizures)
+print('num_seizures = ', num_seizures)	# number of seizures for this patient
+print('preictal time (min) = ', preictal_time/60)	# in min
+print('interictal time (min) = ', interictal_time/60)	# in min
