8b4d79fcb02f0603230447074e5b90c38e3e66b2
jenniferhellar <jenniferhellar@gmail.com>
2022-03-22 14:47:20 -0500

    Update extract_all_seg.py
    extract_all_seg.py now compatible with new extract_seg.py and const.py

--- extract_all_seg.py
+++ extract_all_seg.py
@@ -1,7 +1,25 @@
 import os
+import argparse
 
-from const import LOGDIR, PATIENTS
+import const
 
+
+parser = argparse.ArgumentParser(
+    description='Processes all patients from the input dataset to extract preictal and interictal segments.')
+
+parser.add_argument('--dataset', required=True,
+					help='Dataset to process.')
+
+args = parser.parse_args()
+dataset = vars(args)['dataset']
+
+if dataset not in const.DATASETS:
+	print('\n\nERROR: unsupported dataset argument. Allowed options: \n', const.DATASETS)
+	exit(1)
+
+_, PATIENTS, _ = const.get_details(dataset)
+_, _, LOGDIR = const.get_dirs(dataset)
 
 for p in PATIENTS:
-	os.system('python3 tb_extract_seg.py --patient ' + p + ' 2>&1 | tee ' + os.path.join(LOGDIR, 'log_seg_' + p + '.txt'))
+	logfile = os.path.join(LOGDIR, 'log_seg_{}.txt'.format(p))
+	os.system('python3 extract_seg.py --dataset {} --patient {} 2>&1 | tee {}'.format(dataset, p, logfile))
